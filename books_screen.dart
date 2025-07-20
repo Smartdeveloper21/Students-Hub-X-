b2c5606744ae73cf6c04dc9b17d0e03c8b8037ac
Smartdeveloper21 <165462565+Smartdeveloper21@users.noreply.github.com>
2025-07-20 15:11:22 +0530

    Add files via upload

---
+++ books_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BooksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Books Section'));
+  }
+}

5d2fa65383d4b1e0947c14ac61001a14ca4d3cb5
Smartdeveloper21 <165462565+Smartdeveloper21@users.noreply.github.com>
2025-07-20 15:18:01 +0530

    Delete books_screen.dart

--- books_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BooksScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Books Section'));
-  }
-}
